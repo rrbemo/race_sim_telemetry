58079de44b0780d33e6a17d170a2d8e89625b29b
Ryan Bemowski <rrbemo@gmail.com>
2022-11-06 12:52:28 -0600

    Updated the recorder script to read and save off a local version of all data. Might need to do some alterations so that single quotes are double quotes again. JSON data is not correctly formatted for R usage (jsonlite or rjson).

--- static/recorder.py
+++ static/recorder.py
@@ -1,24 +1,19 @@
-import time
+import socket
+import json
+from packets import (
+    unpack_udp_packet,
+)
 
-from collections import namedtuple
+#UDP_IP = "127.0.0.1"
+UDP_IP = "" # Use this to listen to all incoming UDP traffic on that port.
+UDP_PORT = 22022
 
-TimestampedPacket = namedtuple("TimestampedPacket", "timestamp, packet")
+sock = socket.socket(socket.AF_INET,  # Internet
+                     socket.SOCK_DGRAM)  # UDP
+sock.bind((UDP_IP, UDP_PORT))
 
-
-#sock = socket.socket(socket.AF_INET, # Internet
-#                     socket.SOCK_DGRAM) # UDP
-#sock.bind((UDP_IP, UDP_PORT))
-
-data = "b'\xe6\x07\x01\t\x01\x03`\xb9\xdeM\xff)\xfe\xebN\x826C\x18(\x00\x00\x00\xffBUTN\x10\x08\x00\x00Z\x17-WZ\x17-W'"
-quitflag = False
-while not quitflag:
-    timestamp = time.time()
-
-    # All telemetry UDP packets fit in 2048 bytes with room to spare.
-    packet = sock.recv(2048)
-    timestamped_packet = TimestampedPacket(timestamp, packet)
-
-#while True:
-    #data, addr = sock.recvfrom(2*1024) # buffer size is 1024 bytes
-
-    #print("received message: %s" % print_data)
+with open("static/TestTelem.txt", "w") as telem_file:
+    while True:
+        udp_packet = sock.recv(2048)
+        packet = json.loads(repr(unpack_udp_packet(udp_packet)).replace("\"", "").replace("'", "\""))
+        telem_file.write("%s \n" % repr(packet))
