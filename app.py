04c6cff480952e56f9c4ed463b353ab665a1ef50
Ryan Bemowski <rrbemo@gmail.com>
2024-10-26 23:35:06 -0500

    Updated to accept a lot more packet types. Having an issue keeping up with all the packets. Need to fix that before handling any more data. The fastest way to save may be to save them as raw json and parse them with a different program.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 import pandas as pd
+import plotly.express as px
 import socket
 import threading
 import json
@@ -7,11 +8,14 @@ from simstore.simstore import SimStore
 import simdataclasses.simdataclasses as rsdc
 from packets.packets_f123 import (
     unpack_udp_packet,
+    PacketID
 )
 import yaml
 
 app = Flask(__name__)
 
+# TODO: find a way to handle more packets... listener sends to a different thread?
+# Maybe spawn a thread to handle each type of packet?
 def udp_listener():
     ## UDP listening
     # UDP_IP = "127.0.0.1"
@@ -28,32 +32,52 @@ def udp_listener():
 
         packet = repr(unpack_udp_packet(udp_packet)).replace("\"", "").replace("'", "\"")
         # Save the raw packet like I originally was...
+        # This is fairly fast and seems to allow a lot of data to be stored.
         my_connection.execute_query(f"INSERT INTO raw_packet (json_data) VALUES (%s)", (packet,))
 
-        # Now parse and save the packet into their own tables.
-        json_packet = json.loads(packet)
-        data = None
-        if json_packet:
-            if json_packet['header']:
-                match json_packet['header']['packetId']:
-                    case '4':
-                        # This packet only seems to be created for online lobbies.
-                        data = rsdc.SessionParticipant.packet_to_data(json_packet)
-                    case '6':
-                        data = rsdc.Telemetry.packet_to_data(json_packet)
-                    case '7':
-                        # TODO: build a dataclass for the car damage packet
-                        print("Missed a damage packet")
-                    case '8':
-                        # TODO: build a dataclass for the session outcome packet
-                        print("Missed the outcomes packet")
-                    case _:
-                        print("packet not handled")
+        # Start a thread to process this packet.
+        # threading.Thread(target=process_packet, args=(my_connection, packet,), daemon=True).start()
+        # Or, just run the function without a thread
+        process_packet(my_connection, packet)
 
-        if data:
-            for d in data:
-                # Execute an insert for each data object
-                my_connection.execute_query(d.insert_string())
+def process_packet(conn, packet):
+    # Now parse and save the packet into their own tables.
+    # This is likely much slower and takes more time to save.
+    json_packet = json.loads(packet)
+    data = []
+    if json_packet:
+        if json_packet['header']:
+            packet_id = json_packet['header']['packetId']
+            print("Packet: " + packet_id)
+
+            # TODO: is it possible to start new threads to process and save each packet?
+            # If so, at this point, maybe the rsdc class should have a packet handler
+            # That funtion should be called for each packet and it should save to the database as needed.
+            match packet_id:
+                case '1':
+                    data = rsdc.SessionDetails.packet_to_data(json_packet)
+                case '2':
+                    data = rsdc.LapData.packet_to_data(json_packet)
+                case '4':
+                    # This packet only seems to be created for online lobbies.
+                    data = rsdc.SessionParticipant.packet_to_data(json_packet)
+                case '6':
+                    data = rsdc.Telemetry.packet_to_data(json_packet)
+                case '7':
+                    data = rsdc.CarStatusData.packet_to_data(json_packet)
+                case '8':
+                    print('Attempting to handle outcome packet!')
+                    data = rsdc.SessionOutcome.packet_to_data(json_packet)
+                    data = data + rsdc.SessionTyreStint.packet_to_data(json_packet) # non-nested way to add list items
+                case _:
+                    #print("packet not handled " + packet_id + " :: " + PacketID(int(packet_id)).name)
+                    pass
+
+    if data:
+        for d in data:
+            print(d)
+            # Execute an insert for each data object
+            conn.execute_query(d.insert_string())
 
 @app.route("/")
 def index():
@@ -89,13 +113,24 @@ def show_sessions():
 def show_session_data(session_id):
     # Get some session data
     my_connection = SimStore()
-    session_data = my_connection.execute_query(f"SELECT * FROM car_telemetry WHERE session_uid = '{session_id}'")
+    session_data = my_connection.execute_query(f"SELECT ct.*, "
+                                               f"sp.driver_name "
+                                               f"FROM car_telemetry ct "
+                                               f"LEFT JOIN session_participant sp ON ct.session_uid = sp.session_uid "
+                                               f"  AND ct.participant_index = sp.participant_index "
+                                               f"WHERE ct.session_uid = '{session_id}'")
+
     print(session_data)
 
-    # Display a something
-    template_data = session_data.groupby('participant_index')
+    session_data['driver_name'] = session_data['driver_name'].fillna('')
 
-    return render_template('session_details.html', data=template_data)
+    # Display a something
+    template_data = session_data.groupby(['participant_index', 'driver_name'])
+
+    # build a plot
+    visual = px.line(session_data, title='Throttle', x='session_time', y='throttle', color='participant_index')
+    vis = visual.to_html(full_html=False)
+    return render_template('session_details.html', data=template_data, vis=vis)
 
 if __name__ == '__main__':
     threading.Thread(target=udp_listener, daemon=True).start()
--- simdataclasses/simdataclasses.py
+++ simdataclasses/simdataclasses.py
@@ -32,6 +32,125 @@ class RaceSimData:
 
 
 
+@dataclass
+class LapData(RaceSimData):
+    sessionUID: int
+    participantIndex: int
+    playerOneIndex: int # This is the index of the player (if this = participantIndex, then it is the player's telemetry)
+    frameId: int
+    sessionTime: float
+    lastLapTimeInMS: int
+    currentLapTimeInMS: int
+    sector1TimeInMS: int
+    sector2TimeInMS: int
+    deltaToCarInFrontInMS: int
+    deltaToRaceLeaderInMS: int
+    lapDistance: float
+    totalDistance: float
+    safetyCarDelta: float
+    carPosition: int
+    currentLapNum: int
+    pitStatus: int
+    numPitStops: int
+    sector: int
+    currentLapInvalid: int
+    penalties: int
+    totalWarnings: int
+    cornerCuttingWarnings: int
+    numUnservedDriveThroughPens: int
+    numUnservedStopGoPens: int
+    gridPosition: int
+    driverStatus: int
+    resultStatus: int
+    pitLaneTimerActive: int
+    pitLaneTimeInLaneInMS: int
+    pitStopTimerInMS: int
+    pitStopShouldServePen: int
+
+    _tableName = 'car_lap'
+
+    # Create (a) new data structure(s) for the given packet
+    @staticmethod
+    def packet_to_data(packet):
+        header = packet['header']
+        data = list()
+        for i in range(0, len(packet['lapData'])):
+            this_participant = packet['lapData'][i]
+            # Build this participant
+            this_data = LapData(header['sessionUID'],
+                                i, # participant index
+                                header['playerCarIndex'],
+                                header['frameIdentifier'],
+                                header['sessionTime'],
+                                this_participant['lastLapTimeInMS'],
+                                this_participant['currentLapTimeInMS'],
+                                this_participant['sector1TimeInMS'],
+                                this_participant['sector2TimeInMS'],
+                                this_participant['deltaToCarInFrontInMS'],
+                                this_participant['deltaToRaceLeaderInMS'],
+                                this_participant['lapDistance'],
+                                this_participant['totalDistance'],
+                                this_participant['safetyCarDelta'],
+                                this_participant['carPosition'],
+                                this_participant['currentLapNum'],
+                                this_participant['pitStatus'],
+                                this_participant['numPitStops'],
+                                this_participant['sector'],
+                                this_participant['currentLapInvalid'],
+                                this_participant['penalties'],
+                                this_participant['totalWarnings'],
+                                this_participant['cornerCuttingWarnings'],
+                                this_participant['numUnservedDriveThroughPens'],
+                                this_participant['numUnservedStopGoPens'],
+                                this_participant['gridPosition'],
+                                this_participant['driverStatus'],
+                                this_participant['resultStatus'],
+                                this_participant['pitLaneTimerActive'],
+                                this_participant['pitLaneTimeInLaneInMS'],
+                                this_participant['pitStopTimerInMS'],
+                                this_participant['pitStopShouldServePen'])
+
+            # Append this participant to a list which will be returned
+            data.append(this_data)
+        return data
+
+    def _data_dictionary(self):
+        data_dict = {
+            'session_uid': self.sessionUID,
+            'participant_index': self.participantIndex,
+            'player_one_index': self.playerOneIndex,
+            'frame_id': self.frameId,
+            'session_time': self.sessionTime,
+            'last_lap_time': self.lastLapTimeInMS,
+            'current_lap_time': self.currentLapTimeInMS,
+            'sector_1_time': self.sector1TimeInMS,
+            'sector_2_time': self.sector2TimeInMS,
+            'delta_to_car_in_front': self.deltaToCarInFrontInMS,
+            'delta_to_race_leader': self.deltaToRaceLeaderInMS,
+            'lap_distance': self.lapDistance,
+            'total_distance': self.totalDistance,
+            'safety_car_delta': self.safetyCarDelta,
+            'car_position': self.carPosition,
+            'current_lap_num': self.currentLapNum,
+            'pit_status': self.pitStatus,
+            'num_pit_stops': self.numPitStops,
+            'sector': self.sector,
+            'current_lap_invalid': self.currentLapInvalid,
+            'penalties': self.penalties,
+            'total_warnings': self.totalWarnings,
+            'corner_cutting_warnings': self.cornerCuttingWarnings,
+            'num_unserved_drive_through_pens': self.numUnservedDriveThroughPens,
+            'num_unserved_stop_go_pens': self.numUnservedStopGoPens,
+            'grid_position': self.gridPosition,
+            'driver_status': self.driverStatus,
+            'result_status': self.resultStatus,
+            'pit_lane_timer_active': self.pitLaneTimerActive,
+            'pit_lane_time_in_lane': self.pitLaneTimeInLaneInMS,
+            'pit_stop_timer': self.pitStopTimerInMS,
+            'pit_stop_should_serve_pen': self.pitStopShouldServePen
+        }
+
+        return data_dict
 
 @dataclass
 class Telemetry(RaceSimData):
@@ -217,10 +336,345 @@ class SessionParticipant(RaceSimData):
             'my_team': self.myTeam,
             'race_number': self.raceNumber,
             'nationality': self.nationality,
-            'driver_name': self.name,
-            'your_telemetry': self.yourTelemetry,
-            'show_online_names': self.showOnlineNames,
+            'driver_name': str("\'" + self.name + "\'"),
+            'your_telemetry': bool(self.yourTelemetry),
+            'show_online_names': bool(self.showOnlineNames),
             'platform': self.platform
+        }
+
+        return data_dict
+
+
+@dataclass
+class CarStatusData(RaceSimData):
+    sessionUID: int
+    participantIndex: int
+    playerOneIndex: int
+    frameId: int
+    sessionTime: float
+    tractionControl: int
+    antiLockBrakes: int
+    fuelMix: int
+    frontBrakeBias: int
+    pitLimiterStatus: int
+    fuelInTank: float
+    fuelCapacity: float
+    fuelRemainingLaps: float
+    maxRPM: int
+    idleRPM: int
+    maxGears: int
+    drsAllowed: int
+    drsActivationDistance: int
+    actualTyreCompound: int
+    visualTyreCompound: int
+    tyresAgeLaps: int
+    vehicleFiaFlags: int
+    enginePowerICE: float
+    enginePowerMGUK: float
+    ersStoreEnergy: float
+    ersDeployMode: int
+    ersHarvestedThisLapMGUK: float
+    ersHarvestedThisLapMGUH: float
+    ersDeployedThisLap: float
+    networkPaused: int
+
+    _tableName = 'car_status_data'
+
+    # Create (a) new data structure(s) for the given packet
+    @staticmethod
+    def packet_to_data(packet):
+        header = packet['header']
+        data = list()
+        for i in range(0, len(packet['carStatusData'])):
+            packet_data = packet['carStatusData'][i]
+            # Build this participant
+            this_data = CarStatusData(header['sessionUID'],
+                                i, # participant index
+                                header['playerCarIndex'],
+                                header['frameIdentifier'],
+                                header['sessionTime'],
+                                packet_data['tractionControl'],
+                                packet_data['antiLockBrakes'],
+                                packet_data['fuelMix'],
+                                packet_data['frontBrakeBias'],
+                                packet_data['pitLimiterStatus'],
+                                packet_data['fuelInTank'],
+                                packet_data['fuelCapacity'],
+                                packet_data['fuelRemainingLaps'],
+                                packet_data['maxRPM'],
+                                packet_data['idleRPM'],
+                                packet_data['maxGears'],
+                                packet_data['drsAllowed'],
+                                packet_data['drsActivationDistance'],
+                                packet_data['actualTyreCompound'],
+                                packet_data['visualTyreCompound'],
+                                packet_data['tyresAgeLaps'],
+                                packet_data['vehicleFiaFlags'],
+                                packet_data['enginePowerICE'],
+                                packet_data['enginePowerMGUK'],
+                                packet_data['ersStoreEnergy'],
+                                packet_data['ersDeployMode'],
+                                packet_data['ersHarvestedThisLapMGUK'],
+                                packet_data['ersHarvestedThisLapMGUH'],
+                                packet_data['ersDeployedThisLap'],
+                                packet_data['networkPaused'])
+
+            # Append this participant to a list which will be returned
+            data.append(this_data)
+        return data
+
+    def _data_dictionary(self):
+        data_dict = {
+            'session_uid': self.sessionUID,
+            'participant_index': self.participantIndex,
+            'player_one_index': self.playerOneIndex,
+            'frame_id': self.frameId,
+            'session_time': self.sessionTime,
+            'traction_control': self.tractionControl,
+            'anti_lock_brakes': self.antiLockBrakes,
+            'fuel_mix': self.fuelMix,
+            'front_brake_bias': self.frontBrakeBias,
+            'pit_limiter_status': self.pitLimiterStatus,
+            'fuel_in_tank': self.fuelInTank,
+            'fuel_capacity': self.fuelCapacity,
+            'fuel_remaining_laps': self.fuelRemainingLaps,
+            'max_rpm': self.maxRPM,
+            'idle_rpm': self.idleRPM,
+            'max_gears': self.maxGears,
+            'drs_allowed': self.drsAllowed,
+            'drs_activation_distance': self.drsActivationDistance,
+            'actual_tyre_compound': self.actualTyreCompound,
+            'visual_tyre_compound': self.visualTyreCompound,
+            'tyres_age_laps': self.tyresAgeLaps,
+            'vehicle_fia_flags': self.vehicleFiaFlags,
+            'engine_power_ice': self.enginePowerICE,
+            'engine_power_mguk': self.enginePowerMGUK,
+            'ers_store_engergy': self.ersStoreEnergy,
+            'ers_deploy_mode': self.ersDeployMode,
+            'ers_harvested_this_lap_mguk': self.ersHarvestedThisLapMGUK,
+            'ers_harvested_this_lap_mguh': self.ersHarvestedThisLapMGUH,
+            'ers_deployed_this_lap': self.ersDeployedThisLap,
+            'network_paused': self.networkPaused
+        }
+
+        return data_dict
+
+@dataclass
+class SessionOutcome(RaceSimData):
+    sessionUID: int
+    participantIndex: int
+    playerOneIndex: int
+    position: int
+    numLaps: int
+    gridPosition: int
+    points: int
+    numPitStops: int
+    resultStatus: int
+    bestLapTimeInMS: int
+    totalRaceTime: float
+    penaltiesTime: int
+    numPenalties: int
+
+    _tableName = 'session_outcome'
+
+    # Create (a) new data structure(s) for the given packet
+    @staticmethod
+    def packet_to_data(packet):
+        header = packet['header']
+        data = list()
+        for i in range(0, len(packet['classificationData'])):
+            packet_data = packet['classificationData'][i]
+            # Build this participant
+            this_data = SessionOutcome(header['sessionUID'],
+                                       i, # participant index
+                                       header['playerCarIndex'],
+                                       packet_data['position'],
+                                       packet_data['numLaps'],
+                                       packet_data['gridPosition'],
+                                       packet_data['points'],
+                                       packet_data['numPitStops'],
+                                       packet_data['resultStatus'],
+                                       packet_data['bestLapTimeInMS'],
+                                       packet_data['totalRaceTime'],
+                                       packet_data['penaltiesTime'],
+                                       packet_data['numPenalties'])
+
+            # Append this participant to a list which will be returned
+            data.append(this_data)
+        return data
+
+    def _data_dictionary(self):
+        data_dict = {
+            'session_uid': self.sessionUID,
+            'participant_index': self.participantIndex,
+            'player_one_index': self.playerOneIndex,
+            'pos': self.position,
+            'num_laps': self.numLaps,
+            'grid_position': self.gridPosition,
+            'points': self.points,
+            'num_pit_stops': self.numPitStops,
+            'result_status': self.resultStatus,
+            'best_lap_time': self.bestLapTimeInMS,
+            'total_race_time': self.totalRaceTime,
+            'penalties_time': self.penaltiesTime,
+            'num_penalties': self.numPenalties
+        }
+
+        return data_dict
+
+@dataclass
+class SessionTyreStint(RaceSimData):
+    sessionUID: int
+    participantIndex: int
+    playerOneIndex: int
+    tyreStint: int
+    numTyreStints: int
+    tyreStintActual: int
+    tyreStintVisual: int
+    tyreStintEndLaps: int
+
+    _tableName = 'session_tyre_stint'
+
+    # Create (a) new data structure(s) for the given packet
+    @staticmethod
+    def packet_to_data(packet):
+        header = packet['header']
+        data = list()
+        for i in range(0, len(packet['classificationData'])):
+            packet_data = packet['classificationData'][i]
+            for j in range(0, int(packet_data['numTyreStints'])):
+                this_data = SessionTyreStint(header['sessionUID'],
+                                             i, # participant index
+                                             header['playerCarIndex'],
+                                             j + 1, # stint number
+                                             packet_data['numTyreStints'],
+                                             packet_data['tyreStintsActual'][j],
+                                             packet_data['tyreStintsVisual'][j],
+                                             packet_data['tyreStintsEndLaps'][j])
+
+                # Append this participant to a list which will be returned
+                data.append(this_data)
+
+        return data
+
+    def _data_dictionary(self):
+        data_dict = {
+            'session_uid': self.sessionUID,
+            'participant_index': self.participantIndex,
+            'player_one_index': self.playerOneIndex,
+            'tyre_stint': self.tyreStint,
+            'num_tyre_stints': self.numTyreStints,
+            'tyre_stint_actual': self.tyreStintActual,
+            'tyre_stint_visual': self.tyreStintVisual,
+            'tyre_stint_end_laps': self.tyreStintEndLaps
+        }
+
+        return data_dict
+
+@dataclass
+class SessionDetails(RaceSimData):
+    sessionUID: int
+    frameId: int
+    sessionTime: float
+    weather: int
+    trackTemperature: int
+    airTemperature: int
+    totalLaps: int
+    trackLength: int
+    sessionType: int
+    trackId: int
+    formula: int
+    sessionTimeLeft: int
+    sessionDuration: int
+    pitSpeedLimit: int
+    gamePaused: int
+    safetyCarStatus: int
+    networkGame: int
+    aiDifficulty: int
+    seasonLinkIdentifier: int
+    weekendLinkIdentifier: int
+    sessionLinkIdentifier: int
+    gameMode: int
+    ruleSet: int
+    timeOfDay: int
+    sessionLength: int
+    numSafetyCarPeriods: int
+    numVirtualSafetyCarPeriods: int
+    numRedFlagPeriods: int
+
+    _tableName = 'session_details'
+
+    # Create (a) new data structure(s) for the given packet
+    @staticmethod
+    def packet_to_data(packet):
+        header = packet['header']
+        data = list()
+        packet_data = packet
+        # Build this participant
+        this_data = SessionDetails(header['sessionUID'],
+                                   header['frameIdentifier'],
+                                   header['sessionTime'],
+                                   packet_data['weather'],
+                                   packet_data['trackTemperature'],
+                                   packet_data['airTemperature'],
+                                   packet_data['totalLaps'],
+                                   packet_data['trackLength'],
+                                   packet_data['sessionType'],
+                                   packet_data['trackId'],
+                                   packet_data['formula'],
+                                   packet_data['sessionTimeLeft'],
+                                   packet_data['sessionDuration'],
+                                   packet_data['pitSpeedLimit'],
+                                   packet_data['gamePaused'],
+                                   packet_data['safetyCarStatus'],
+                                   packet_data['networkGame'],
+                                   packet_data['aiDifficulty'],
+                                   packet_data['seasonLinkIdentifier'],
+                                   packet_data['weekendLinkIdentifier'],
+                                   packet_data['sessionLinkIdentifier'],
+                                   packet_data['gameMode'],
+                                   packet_data['ruleSet'],
+                                   packet_data['timeOfDay'],
+                                   packet_data['sessionLength'],
+                                   packet_data['numSafetyCarPeriods'],
+                                   packet_data['numVirtualSafetyCarPeriods'],
+                                   packet_data['numRedFlagPeriods'])
+
+        #print(this_data)
+        # Append this participant to a list which will be returned
+        data.append(this_data)
+        return data
+
+    def _data_dictionary(self):
+        data_dict = {
+            'session_uid': self.sessionUID,
+            'frame_id': self.frameId,
+            'session_time': self.sessionTime,
+            'weather': self.weather,
+            'track_temperature': self.trackTemperature,
+            'air_temperature': self.airTemperature,
+            'total_laps': self.totalLaps,
+            'track_length': self.trackLength,
+            'session_type': self.sessionType,
+            'track_id': self.trackId,
+            'formula': self.formula,
+            'session_time_left': self.sessionTimeLeft,
+            'session_duration': self.sessionDuration,
+            'pit_speed_limit': self.pitSpeedLimit,
+            'game_paused': self.gamePaused,
+            'safety_car_status': self.safetyCarStatus,
+            'network_game': self.networkGame,
+            'ai_difficulty': self.aiDifficulty,
+            'season_link_identifier': self.seasonLinkIdentifier,
+            'weekend_link_identifier': self.weekendLinkIdentifier,
+            'session_link_identifier': self.sessionLinkIdentifier,
+            'game_mode': self.gameMode,
+            'rule_set': self.ruleSet,
+            'time_of_day': self.timeOfDay,
+            'session_length': self.sessionLength,
+            'num_safety_car_periods': self.numSafetyCarPeriods,
+            'num_virtual_safety_car_periods': self.numVirtualSafetyCarPeriods,
+            'num_red_flag_periods': self.numRedFlagPeriods
         }
 
         return data_dict
